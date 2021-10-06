85e38fc16a346678acad24e66ba6558194a8b60f
CheboiDerrick <derrick.cheboi@student.moringaschool.com>
2021-10-06 12:38:03 +0300

    fixed git guardian

--- tribune/settings.py
+++ tribune/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9(tlp*k1!1e2m_4cn7t=t!14+!-%xj2tyvx7lt#bw(42p%tsay'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
