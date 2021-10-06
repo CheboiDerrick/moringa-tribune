599a3d3c67ef9342ab0d79fb916e0fab951f8d0d
CheboiDerrick <derrick.cheboi@student.moringaschool.com>
2021-10-06 10:18:37 +0300

    deployment

--- tribune/settings.py
+++ tribune/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-9(tlp*k1!1e2m_4cn7t=t!14+!-%xj2tyvx7lt#bw(42p%tsay
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['tribune-77.herokuapp.com']
+ALLOWED_HOSTS = ['']
 
 
 # Application definition
@@ -130,28 +130,9 @@ STATICFILES_DIRS = [
 ]
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.11/howto/static-files/
-# PROJECT_ROOT   =   os.path.join(os.path.abspath(__file__))
-# STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'staticfiles')
-# STATIC_URL = '/static/'
-
-# Extra lookup directories for collectstatic to find static files
-# STATICFILES_DIRS = (
-#     os.path.join(PROJECT_ROOT, 'static'),
-# )
-
-#  Add configuration for static files storage using whitenoise
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-
-# import dj_database_url 
-# prod_db  =  dj_database_url.config(conn_max_age=500)
-# DATABASES['default'].update(prod_db)
